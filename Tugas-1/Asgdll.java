1703c5f39413a8e56b80417e4735ac86117956ad
stnurhalimaa <halimahdrr160@gmail.com>
2025-02-26 21:36:23 +0800

    Menambahkan Sub Folder

---
+++ Tugas-1/Asgdll.java
@@ -0,0 +1,10 @@
+public class Asgdll {
+	public static void main(String[] args) {
+	float f= 20.0f; 
+	double fll;
+
+	fll=10.0f;
+	System.out.println ("f : "+f +
+					"\nf11: "+fll);
+	}
+}
---
+++ Tugas-1/Bacakar.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bacakar {
+    public static void main(String[] args) throws IOException {
+        char cc;
+        int bil;
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader dataIn = new BufferedReader(isr);
+        BufferedReader datAIn = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("hello\n");
+
+        System.out.print("baca 1 karakter : ");
+        cc = dataIn.readLine().charAt(0);
+
+        System.out.print("baca 1 bilangan : ");
+        bil = Integer.parseInt(datAIn.readLine());
+
+        System.out.println(cc);
+        System.out.println(bil);
+        System.out.println("bye");
+    }
+}
